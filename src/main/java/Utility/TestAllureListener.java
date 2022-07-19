bbbe1b73953fb03eb0aa36c664b7be8f30ba95a9
Sardar Khan <sardarkh@exceedgulf.com>
2022-07-19 17:14:34 +0500

    new commit

--- src/main/java/Utility/TestAllureListener.java
+++ src/main/java/Utility/TestAllureListener.java
@@ -35,28 +35,23 @@ public class TestAllureListener implements ITestListener {
 		return html;
 	}
 
-	@Override
 	public void onStart(ITestContext iTestContext) {
 		System.out.println("I am in onStart method " + iTestContext.getName());
 		iTestContext.setAttribute("WebDriver", Test_Base.getDriver());
 	}
 
-	@Override
 	public void onFinish(ITestContext iTestContext) {
 		System.out.println("I am in onFinish method " + iTestContext.getName());
 	}
 
-	@Override
 	public void onTestStart(ITestResult iTestResult) {
 		System.out.println("I am in onTestStart method " + getTestMethodName(iTestResult) + " start");
 	}
 
-	@Override
 	public void onTestSuccess(ITestResult iTestResult) {
 		System.out.println("I am in onTestSuccess method " + getTestMethodName(iTestResult) + " succeed");
 	}
 
-	@Override
 	public void onTestFailure(ITestResult iTestResult) {
 		System.out.println("I am in onTestFailure method " + getTestMethodName(iTestResult) + " failed");
 		Object testClass = iTestResult.getInstance();
@@ -70,12 +65,10 @@ public class TestAllureListener implements ITestListener {
 		saveTextLog(getTestMethodName(iTestResult) + " failed and screenshot taken!");		
 	}
 
-	@Override
 	public void onTestSkipped(ITestResult iTestResult) {
 		System.out.println("I am in onTestSkipped method " + getTestMethodName(iTestResult) + " skipped");
 	}
 
-	@Override
 	public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
 		System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
 	}
