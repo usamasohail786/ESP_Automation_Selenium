53c6120ebb07d997e162a5e58f4b28a306129c2e
Sardar Khan <sardarkh@exceedgulf.com>
2022-07-22 18:41:16 +0500

    error

--- src/main/java/Pages/Log_In_Stem_ex.java
+++ src/main/java/Pages/Log_In_Stem_ex.java
@@ -54,6 +54,7 @@ public class Log_In_Stem_ex {
     	password_input_text.sendKeys(pass);
     	sign_in_btn.click();	
     	com.Explicit_wait_elementToBeVisible(error_pass, 5);
+    	System.out.println(error_pass.getText()+"error");
     	com.Explicit_wait_elementToBeInvisible(spinner, 100);
     	
     }
